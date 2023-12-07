5c59314b90d6d33d8c8b2bfb9136a0397129382a
Md. Abu Bakar Siddique Sadi <sadiakash1309@gmail.com>
2023-12-07 16:12:54 +0600

    Issue solved

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", function () {
-  // Get elements
-  const changeBtn = document.querySelector(".change");
-  const bodyEl = document.querySelector("body");
-
-  // Bind click event
-  changeBtn.addEventListener("click", function () {
-    bodyEl.classList.toggle("dark");
-
-    if (bodyEl.classList.contains("dark")) {
-      changeBtn.textContent = "ON";
-    } else {
-      changeBtn.textContent = "OFF";
-    }
-  });*/
-
 document.addEventListener("DOMContentLoaded", function () {
   // Get elements
   const changeBtn = document.querySelector(".change");
@@ -46,5 +30,51 @@ document.addEventListener("DOMContentLoaded", function () {
         controlDiv.style.backgroundColor = "#fefefe";
       }
     });
+
+    controlDiv.addEventListener("mouseover", function () {
+      controlDiv.style.backgroundColor = "#cccccc"; // Change to desired hover color
+    });
+
+    controlDiv.addEventListener("mouseout", function () {
+      if (status.textContent === "OFF") {
+        controlDiv.style.backgroundColor = "#fefefe";
+      } else {
+        controlDiv.style.backgroundColor = "#99ccff";
+      }
+    });
   });
 });
+
+/*document.addEventListener("DOMContentLoaded", function () {
+  // Get elements
+  const changeBtn = document.querySelector(".change");
+  const bodyEl = document.querySelector("body");
+
+  // Bind click event
+  changeBtn.addEventListener("click", function () {
+    bodyEl.classList.toggle("dark");
+
+    if (bodyEl.classList.contains("dark")) {
+      changeBtn.textContent = "ON";
+    } else {
+      changeBtn.textContent = "OFF";
+    }
+  });*/
+
+/* const controlDivs = document.querySelectorAll(".control");
+
+  controlDivs.forEach((controlDiv) => {
+    const status = controlDiv.querySelector(".status");
+
+    controlDiv.addEventListener("click", function () {
+      if (status.textContent === "OFF") {
+        status.textContent = "ON";
+        controlDiv.style.backgroundColor = "#99ccff";
+      } else {
+        status.textContent = "OFF";
+        controlDiv.style.backgroundColor = "#fefefe";
+      }
+      
+    });
+  });
+}); */
