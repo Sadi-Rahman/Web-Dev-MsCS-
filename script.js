5ebc195201d447150bbb3fc3ba40023fe3b02a73
Md. Abu Bakar Siddique Sadi <sadiakash1309@gmail.com>
2023-12-07 14:30:49 +0600

    Almost

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Wait for DOM to load
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
   // Get elements
   const changeBtn = document.querySelector(".change");
   const bodyEl = document.querySelector("body");
@@ -13,5 +12,39 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       changeBtn.textContent = "OFF";
     }
+  });*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get elements
+  const changeBtn = document.querySelector(".change");
+  const bodyEl = document.querySelector("body");
+
+  // Bind click event
+  changeBtn.addEventListener("click", function () {
+    bodyEl.classList.toggle("dark");
+
+    if (bodyEl.classList.contains("dark")) {
+      changeBtn.textContent = "ON";
+      bodyEl.style.backgroundImage = "url('/image/black.png')";
+    } else {
+      changeBtn.textContent = "OFF";
+      bodyEl.style.backgroundImage = "url('/image/back.png')";
+    }
+  });
+
+  const controlDivs = document.querySelectorAll(".control");
+
+  controlDivs.forEach((controlDiv) => {
+    const status = controlDiv.querySelector(".status");
+
+    controlDiv.addEventListener("click", function () {
+      if (status.textContent === "OFF") {
+        status.textContent = "ON";
+        controlDiv.style.backgroundColor = "#99ccff";
+      } else {
+        status.textContent = "OFF";
+        controlDiv.style.backgroundColor = "#fefefe";
+      }
+    });
   });
 });
